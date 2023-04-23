0006a85f5c54dd663c1d3a26494506ac40f6fce9
NRGLook <ilya.tihanyonok@yandex.by>
2023-04-23 20:24:31 +0300

    add constansts file according to the response function

--- OOP/OOP_PrioritizeMyFuture_2023/var/Constants.py
+++ OOP/OOP_PrioritizeMyFuture_2023/var/Constants.py
@@ -1,25 +1,26 @@
 GREETING_TEXT = "Welcome to the app that will help you manage your time and stop wasting it!"
 
 OPTIONS_TODO = """
-            007 - show all tasks
-            0 - show single task
             1 - add task(description of the task)
             2 - remove task
             3 - update task
-            4 - change styles
-            5 - set hashtag of the task
-            6 - set y.o. for your task
-            7 - get y.o. for the task
+            4 - show_all_tasks
+            5 - show_specific_task
+            6 - show_done_task
+            7 - show_not_done_task
             8 - burn today 
             9 - transfer_to_future
             10 - exit
             """
 
-OPTIONS_FOR_SINGLE_TASK = """DONE
- NOT DONE
- IN PROCESS(ilya procrastinator doing smth else)"""
+OPTIONS_FOR_SINGLE_TASK = """
+                            DONE
+                            NOT DONE
+                            IN PROCESS
+                            """
 
-LIST_OF_ITEM_SHOP = """1 - book
-                       2 - free subscribe for 1 month
-                       3 - other items in developing
+LIST_OF_ITEM_SHOP = """
+                    1 - book
+                    2 - free subscribe for 1 month
+                    3 - other items in developing
                     """
